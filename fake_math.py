c7e5e144adc241b7e2fb34656d475bbd103d2d86
VladimirCh67 <chernenko.vb@gmail.com>
2024-11-04 20:54:52 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,7 @@
+def divide(f, s):
+    if s == 0:
+        res = 'Ошибка: деление на ноль'
+    else:
+        res = f / s
+
+    return res
---
+++ true_math.py
@@ -0,0 +1,10 @@
+from math import inf
+
+def divide(f, s):
+    if s == 0:
+        res = inf
+    else:
+        res = f / s
+
+    return res
+
